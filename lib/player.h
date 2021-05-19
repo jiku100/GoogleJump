bab1df9fa348f494971dd90de9a406da9dea2676
jiku100 <tlstjrrud007@naver.com>
2021-05-19 23:38:25 +0900

    Add Linked List

--- lib/player.h
+++ lib/player.h
@@ -10,6 +10,7 @@ typedef struct Player{
     int stage;
     int score;
     int life;
+    struct Player* next;
 } Player;
 
 /****************** initiation *******************/
@@ -19,18 +20,23 @@ Player* init_player();  // Init Player class
 void setName(Player* p, char name[]);   // Set player name
 void setStage(Player* p, int stage);    // Set player stage
 void setScore(Player* p, int score);    // Set player score
-void setLift(Player* p, int life);      // Set player life
+void setLife(Player* p, int life);      // Set player life
 void setAll(Player* p, char name[], int stage, int score, int life);    // Set All 
 
 /******************  Getter **********************/
 char* getName(Player* p);   // Get player name
 int getStage(Player* p);    // Get player stage
 int getScore(Player* p);    // Get player score
-int getLift(Player* p);     // Get player life
+int getLife(Player* p);     // Get player life
 
 /****************** Actions **********************/
-void liftDown(Player* p);   // Life down
+void lifeDown(Player* p);   // Life down
 void addScore(Player* p, int score);    // Add score
 void nextScore(Player* p);  // Next stage
 void printPlayerInfo(Player* p);    // Print Player info
+
+/**************** Linked List ********************/
+Player* create_node(char name[], int stage, int score, int life);   // create Node
+Player* append(Player* head, Player* node); // append Node
+void printPlayerList(Player* head); // print player list;
 #endif
--- src/player.c
+++ src/player.c
@@ -7,6 +7,7 @@ Player* init_player(){
     p->stage = 0;
     p->score = 0;
     p->life = 0;
+    p->next = NULL;
     return p;
 }
 
@@ -20,14 +21,14 @@ void setStage(Player* p, int stage){
 void setScore(Player* p, int score){
     p->score = score;
 }
-void setLift(Player* p, int life){
+void setLife(Player* p, int life){
     p->life = life;
 }
 void setAll(Player* p, char name[], int stage, int score, int life){
     setName(p, name);
     setStage(p, stage);
     setScore(p, score);
-    setLift(p, life);
+    setLife(p, life);
 }
 
 /******************  Getter **********************/
@@ -42,12 +43,12 @@ int getStage(Player* p){
 int getScore(Player* p){
     return p->score;
 }
-int getLift(Player* p){
+int getLife(Player* p){
     return p->life;
 }
 
 /****************** Actions **********************/
-void liftDown(Player* p){
+void lifeDown(Player* p){
     p->life--;
 }
 void addScore(Player* p, int score){
@@ -61,4 +62,45 @@ void printPlayerInfo(Player* p){
     printf("Stage: %d\n", p->stage);
     printf("Score: %d\n", p->score);
     printf("Life: %d\n", p->life);    
+}
+
+/**************** Linked List ********************/
+Player* create_node(char name[], int stage, int score, int life){
+    Player* node = (Player*)malloc(sizeof(Player));
+	setName(node, name);
+    setStage(node, stage);
+    setScore(node, score);
+    setLife(node, life);
+    node->next = NULL;
+	return node;
+}
+
+Player* append(Player* head, Player* node){
+    Player* nextNode = head;
+
+	if (head == NULL) {
+		head = node;
+		return head;
+	}
+
+	while (nextNode->next != NULL) {
+		nextNode = nextNode->next;
+	}
+
+	nextNode->next = node;
+
+	return head;
+}
+
+void printPlayerList(Player* head){
+    if (head == NULL) {
+		printf("There is no node!!\n");
+	}
+	else {
+		while(head != NULL)
+		{
+			printf("%s\t%d\t%d\t%d\n", head->name, head->stage, head->score, head->life);
+			head = head->next;
+		}
+	}
 }
